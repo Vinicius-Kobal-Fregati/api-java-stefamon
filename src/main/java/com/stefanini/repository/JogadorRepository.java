a7209c71c9900ef3bc43f3aa7cc4d89eb4efb0f7
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-02-26 14:05:01 -0300

    Criacao do metodo de login de jogador no repository

--- src/main/java/com/stefanini/repository/JogadorRepository.java
+++ src/main/java/com/stefanini/repository/JogadorRepository.java
@@ -1,10 +1,10 @@
 package com.stefanini.repository;
 
 import com.stefanini.dao.GenericDAO;
-import com.stefanini.dto.JogadorCadastroDTO;
 import com.stefanini.entity.Jogador;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @ApplicationScoped
@@ -27,5 +27,12 @@ public class JogadorRepository extends GenericDAO<Jogador, Long> {
 
     public List<Jogador> listarTodos() {
         return listAll();
+    }
+
+    public TypedQuery<Jogador> loginDoJogador(String nickname, String password) {
+        return createQuery("FROM Jogador WHERE nickname = :nickname AND " +
+                "password = :password")
+                .setParameter("nickname", nickname)
+                .setParameter("password", password);
     }
 }
