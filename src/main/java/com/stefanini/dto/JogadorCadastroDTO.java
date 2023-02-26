ea8b2e5e355e96351a1678ac9c861450b4263c5f
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-02-26 13:56:11 -0300

    Implementacao da logica do JogadorRepository e criacao de sua DTO

---
+++ src/main/java/com/stefanini/dto/JogadorCadastroDTO.java
@@ -0,0 +1,7 @@
+package com.stefanini.dto;
+
+public class JogadorCadastroDTO {
+    private Long id;
+    private String nickname;
+    private String password;
+}
--- src/main/java/com/stefanini/repository/JogadorRepository.java
+++ src/main/java/com/stefanini/repository/JogadorRepository.java
@@ -1,10 +1,31 @@
 package com.stefanini.repository;
 
 import com.stefanini.dao.GenericDAO;
+import com.stefanini.dto.JogadorCadastroDTO;
 import com.stefanini.entity.Jogador;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class JogadorRepository extends GenericDAO<Jogador, Long> {
+    public void salvar(Jogador jogador) {
+        this.save(jogador);
+    }
+
+    public Jogador pegarPorId(Long id) {
+        return findById(id);
+    }
+
+    public void alterar(Jogador jogador) {
+        this.update(jogador);
+    }
+
+    public void deletar(Long id) {
+        delete(id);
+    }
+
+    public List<Jogador> listarTodos() {
+        return listAll();
+    }
 }
