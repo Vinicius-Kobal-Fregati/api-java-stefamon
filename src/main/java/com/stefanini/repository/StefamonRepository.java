6a7f48aa40b8827c44a4f128975547a95599bf05
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-02-26 14:09:28 -0300

    Criacao dos metodos do repository do stefamon

--- src/main/java/com/stefanini/repository/StefamonRepository.java
+++ src/main/java/com/stefanini/repository/StefamonRepository.java
@@ -5,7 +5,16 @@ import com.stefanini.dao.GenericDAO;
 import com.stefanini.entity.Stefamon;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class StefamonRepository extends GenericDAO<Stefamon, Long> {
+
+    public List<Stefamon> listarTodos() {
+        return listAll();
+    }
+
+    public Stefamon pegarPorId(Long id) {
+        return findById(id);
+    }
 }
